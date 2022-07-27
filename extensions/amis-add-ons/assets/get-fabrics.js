0e51be181f461b60a81060d1873c514f58285f99
Kaylin Punotai <kaylin.punotai@gmail.com>
2022-07-27 11:14:04 -0500

    Extension connected to API
    Successfully connected extension to API

--- extensions/amis-add-ons/assets/get-fabrics.js
+++ extensions/amis-add-ons/assets/get-fabrics.js
@@ -1,6 +1,10 @@
 // Get fabrics and render Action Select for customer fabric selection
 // Loaded in fabric-selector.liquid
 
+
+///// WIP /////
+// - add material filters
+
 (function () {
   // get a specific element from liquid
   function getSelect(selector) {
@@ -9,21 +13,44 @@
 
   // get all fabrics filtered by allowed materials
   function getFabrics(filters) {
-    const url = "/apps/api/fabric_entries/index"; // "/api/fabric_entries/filter";
+    const proxySubpath = "/apps/api";
+    const url = proxySubpath + "/api/fabric_entries/index"; // "/api/fabric_entries/filter";
     const method = "GET"; // "POST"
     const headers = { "Content-Type": "application/json" };
     const body = ""; // {"fabric_entry": {"material": filters}};
     return fetch(url); // return fetch(url, {method: method, headers: headers, body: JSON.stringify(body),});
   }
 
+  // creates an option inside the select input
+  function createOption(fabric) {
+    const option = document.createElement("option");
+    option["value"] = fabric;
+    option.innerHTML = fabric;
+
+    return option;  
+  }
+
   // set options for customers to select using fabric list
   async function setOptions() {
+    const debug = false;
     const select = getSelect(".fabric-selector");
-    const result = await getFabrics("filter");
-    if (result.ok) {
-      console.log("ok");
+
+    if (debug) {
+      const fabrics = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"].sort();
+      fabrics.forEach( (fabric) => {
+        select.appendChild(createOption(fabric));
+      });
+    } else {
+      const result = await getFabrics("filter");
+      if (result.ok) {
+        const fabrics = await result.json();
+        console.log(fabrics);
+        fabrics.forEach( (fabric) => {
+          select.appendChild(createOption(fabric.title));
+        });
+      }
+      return { status: "success" };
     }
-    return { status: "success" };
   }
 
   setOptions();

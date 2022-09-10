1b7f458e88030aff54aca2617c6f8a78b18b3bb4
Kaylin Punotai <kaylin.punotai@gmail.com>
2022-09-10 13:53:14 -0500

    Migrate to Fly.io
    Migrating webhost to Fly.io and database host to ElephantSQL because Heroku is ending free trials :(

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Shopify App for Ami's Creations US
 
-A shopify app hosted on Heroku that stores fabric options and generates extension blocks specific to the merchant. 
+A shopify app hosted on Fly.io that stores fabric options and generates extension blocks specific to the merchant. 
--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -6,9 +6,13 @@ class HomeController < ApplicationController
   include ShopifyApp::ShopAccessScopesVerification
 
   DEV_INDEX_PATH = Rails.root.join("frontend")
-  PROD_INDEX_PATH = Rails.public_path.join("dist")
+  # PROD_INDEX_PATH = Rails.public_path.join("dist")
+  PROD_INDEX_PATH = "/app/frontend/dist"
 
   def index
+    puts PROD_INDEX_PATH
+    puts DEV_INDEX_PATH
+
     contents = File.read(File.join(Rails.env.production? ? PROD_INDEX_PATH : DEV_INDEX_PATH, "index.html"))
 
     render(plain: contents, content_type: "text/html", layout: false)
